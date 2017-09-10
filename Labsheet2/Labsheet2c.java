eabb0ccd92d8bb94c8587232dad2f28dd9b0df76
Shashvat Kedia <shashvat51@gmail.com>
2017-09-11 01:06:28 +0530

    Update Labsheet2c.java

--- Labsheet2/Labsheet2c.java
+++ Labsheet2/Labsheet2c.java
@@ -3,8 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package labsheet3;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
